7a625c961b458b2ce20959ad971faaeaaba8be4f
Belén Victorio <victoriobell@gmail.com>
2022-09-09 00:40:12 -0300

    trabajo en react - mi primera app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import './App.css';
+import SideBar from './components/SideBar';
+import ContentWrapper from './components/ContentWrapper';
+
+
+const App = () => {
+  return ( // siempre retornar entre parentesis 
+  //debe haber un elemento padre como caja y dentro de ella meter lo demás
+  <div id="wrapper">
+
+    <SideBar/>
+    <ContentWrapper/>
+
+</div>
   );
 }
 
